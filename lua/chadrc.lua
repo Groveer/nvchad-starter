f73e535b1e649985fcd4572a71f95a266c9b4e7d
groveer <groveer@qq.com>
2024-03-10 23:00:53 +0800

    init persional config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,32 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    Normal = {
+      bg = { "black", 3 },
+    },
+  },
+  theme = "oxocarbon",
+  transparency = true,
+  nvdash = {
+    load_on_startup = true,
+  },
+  lsp = {
+    signature = false,
+    hover = false,
+    semantic_tokens = false,
+  },
+  term = {
+    -- hl = "Normal:term,WinSeparator:WinSeparator",
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.25,
+      width = 1,
+      height = 1,
+      border = "single",
+    },
+  },
 }
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.cursorlineopt ='both' -- to enable cursorline!
+-- filetype for plantuml
+vim.api.nvim_command "au BufNewFile,BufRead *.puml setfiletype puml"
+vim.api.nvim_command "au BufNewFile,BufRead *.pu setfiletype puml"
+vim.api.nvim_command "au BufNewFile,BufRead *.plantuml setfiletype puml"
+vim.api.nvim_command "au BufNewFile,BufRead *.uml setfiletype puml"
+vim.api.nvim_command "au BufNewFile,BufRead *.iuml setfiletype puml"
+-- filetype for qml
+vim.api.nvim_command "au BufNewFile,BufRead *.qml setfiletype qmljs"
+
+vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snips"
+vim.o.listchars=[[tab:>\ ,trail:·,eol:↲]]
+vim.o.list = true
