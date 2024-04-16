63adf8ec1ec9ab2ca8a578a816cbc3b9b83da1d1
groveer <groveer@qq.com>
2024-04-16 10:09:12 +0800

    feat: add `neovide` config

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
-vim.o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.cursorlineopt = "both" -- to enable cursorline!
 -- filetype for plantuml
 vim.api.nvim_command "au BufNewFile,BufRead *.puml setfiletype plantuml"
 vim.api.nvim_command "au BufNewFile,BufRead *.pu setfiletype plantuml"
@@ -13,3 +13,12 @@ vim.api.nvim_command "au BufNewFile,BufRead *.iuml setfiletype plantuml"
 vim.api.nvim_command "au BufNewFile,BufRead *.qml setfiletype qmljs"
 
 vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snips"
+
+if vim.g.neovide then
+  vim.o.guifont = "Maple Mono SC NF:h14"
+  vim.g.neovide_transparency = 1.0
+  vim.g.neovide_input_ime = false
+  vim.g.neovide_scroll_animation_length = 0.2
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
