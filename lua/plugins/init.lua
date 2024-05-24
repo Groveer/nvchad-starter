9a47681960d74ec3f090c6ee99aa45a34d2c7d11
groveer <groveer@qq.com>
2024-05-24 20:45:20 +0800

    chore: remove some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,11 @@
 local global = require "global"
 
 return {
+  -- disable
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
+  },
   -- overrides
   {
     "neovim/nvim-lspconfig",
@@ -232,30 +237,6 @@ return {
     end,
     config = function(_, opts)
       return require("crates").setup(opts)
-    end,
-  },
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1001, -- this plugin needs to run before anything else
-    lazy = false,
-    opts = {
-      rocks = { "magick" },
-    },
-  },
-  {
-    "brenoprata10/nvim-highlight-colors",
-    event = "BufRead",
-    config = function(_, opts)
-      require("nvim-highlight-colors").setup(opts)
-    end,
-  },
-  {
-    "3rd/image.nvim",
-    ft = { "png", "jpg", "svg", "gif", "webp" },
-    event = "BufRead",
-    cond = global.is_linux,
-    config = function(_, opts)
-      require("image").setup(opts)
     end,
   },
 }
