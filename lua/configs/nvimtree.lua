1f7c2d45168a1c6bf92b53185440ff2ed37a4cd1
groveer <groveer@outlook.com>
2024-06-18 19:40:12 +0800

    chore: update `nvim-tree` icon

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -5,10 +5,36 @@ return {
   },
 
   renderer = {
-    highlight_git = true,
     icons = {
       show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
         git = true,
+      },
+
+      glyphs = {
+        default = "󰈚",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "󰞋",
+          deleted = "",
+          ignored = "◌",
+        },
       },
     },
   },
