177c96c1089a6137554b57c036e4ba9304915adf
groveer <groveer@outlook.com>
2024-06-30 17:53:17 +0800

    refactor: use local variable for options

--- lua/options.lua
+++ lua/options.lua
@@ -1,24 +1,28 @@
 require "nvchad.options"
 
+local o = vim.o
+local g = vim.g
+local command = vim.api.nvim_command
+local fn = vim.fn
+
 -- add yours here!
-
-vim.o.cursorlineopt = "both" -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 -- filetype for plantuml
-vim.api.nvim_command "au BufNewFile,BufRead *.puml setfiletype plantuml"
-vim.api.nvim_command "au BufNewFile,BufRead *.pu setfiletype plantuml"
-vim.api.nvim_command "au BufNewFile,BufRead *.plantuml setfiletype plantuml"
-vim.api.nvim_command "au BufNewFile,BufRead *.uml setfiletype plantuml"
-vim.api.nvim_command "au BufNewFile,BufRead *.iuml setfiletype plantuml"
+command "au BufNewFile,BufRead *.puml setfiletype plantuml"
+command "au BufNewFile,BufRead *.pu setfiletype plantuml"
+command "au BufNewFile,BufRead *.plantuml setfiletype plantuml"
+command "au BufNewFile,BufRead *.uml setfiletype plantuml"
+command "au BufNewFile,BufRead *.iuml setfiletype plantuml"
 -- filetype for qml
-vim.api.nvim_command "au BufNewFile,BufRead *.qml setfiletype qmljs"
+command "au BufNewFile,BufRead *.qml setfiletype qmljs"
 
-vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snips"
+g.vscode_snippets_path = fn.stdpath "config" .. "/snips"
 
-if vim.g.neovide then
-  vim.o.guifont = "Maple Mono NF CN:h14"
-  vim.g.neovide_transparency = 1.0
-  vim.g.neovide_input_ime = false
-  vim.g.neovide_scroll_animation_length = 0.2
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_cursor_vfx_mode = "torpedo"
+if g.neovide then
+  o.guifont = "Maple Mono NF CN:h14"
+  g.neovide_transparency = 1.0
+  g.neovide_input_ime = false
+  g.neovide_scroll_animation_length = 0.2
+  g.neovide_hide_mouse_when_typing = true
+  g.neovide_cursor_vfx_mode = "torpedo"
 end
