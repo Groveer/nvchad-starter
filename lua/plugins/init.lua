c99fcd82fcd89e4f9118bf6c3314d883e6deb096
groveer <groveer@outlook.com>
2024-07-11 09:18:54 +0800

    fix: comment linewise incorrect

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -53,9 +53,18 @@ return {
   {
     "numToStr/Comment.nvim",
     event = "BufRead",
-    config = function()
+    opts = function()
+      return {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+    config = function(_, opts)
+      require("Comment").setup(opts)
       require("Comment.ft").set("qmljs", "//%s")
     end,
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
   },
 
   -- add plugins to here
