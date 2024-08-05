626e6941860d793e96d8b1752ffebda6399d805b
groveer <groveer@outlook.com>
2024-08-05 09:28:16 +0800

    chore: update markdown config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -69,8 +69,16 @@ return {
   {
     "MeanderingProgrammer/markdown.nvim",
     main = "render-markdown",
-    opts = {},
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    opts = {
+      file_types = { 'markdown', 'vimwiki' },
+      latex = { enabled = false }
+    },
+    ft = { "markdown", "vimwiki" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      vim.treesitter.language.register('markdown', 'vimwiki')
+      require('render-markdown').enable()
+    end,
     -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
