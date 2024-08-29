2e10c1002c4c3257e2279a76a261f19eceb10ab3
groveer <groveer@outlook.com>
2024-08-29 23:29:31 +0800

    chore: update avante config

--- lua/plugins/add/avante.lua
+++ lua/plugins/add/avante.lua
@@ -1,22 +1,143 @@
+local H = {}
+
+H.parse_message = function(opts)
+  local user_prompt = opts.base_prompt
+    .. "\n\nCODE:\n"
+    .. "```"
+    .. opts.code_lang
+    .. "\n"
+    .. opts.code_content
+    .. "\n```"
+    .. "\n\nQUESTION:\n"
+    .. opts.question
+
+  if opts.selected_code_content ~= nil then
+    user_prompt = opts.base_prompt
+      .. "\n\nCODE CONTEXT:\n"
+      .. "```"
+      .. opts.code_lang
+      .. "\n"
+      .. opts.code_content
+      .. "\n```"
+      .. "\n\nCODE:\n"
+      .. "```"
+      .. opts.code_lang
+      .. "\n"
+      .. opts.selected_code_content
+      .. "\n```"
+      .. "\n\nQUESTION:\n"
+      .. opts.question
+  end
+  local user_content = user_prompt
+  return {
+    { role = "system", content = opts.system_prompt },
+    { role = "user", content = user_content },
+  }
+end
+
+H.parse_response = function(data_stream, _, opts)
+  if data_stream:match '"%[DONE%]":' then
+    opts.on_complete(nil)
+    return
+  end
+  if data_stream:match '"delta":' then
+    local json = vim.json.decode(data_stream)
+    if json.choices and json.choices[1] then
+      local choice = json.choices[1]
+      if choice.finish_reason == "stop" then
+        opts.on_complete(nil)
+      elseif choice.delta.content then
+        opts.on_chunk(choice.delta.content)
+      end
+    end
+  end
+end
+
 return {
   "yetone/avante.nvim",
   keys = {
-    { "<leader>aa", mode = { "n", "v" } },
-    { "<leader>ar", mode = { "n", "v" } },
+    {
+      "<leader>aa",
+      function()
+        require("avante.api").ask()
+      end,
+      desc = "avante: ask",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ar",
+      function()
+        require("avante.api").refresh()
+      end,
+      desc = "avante: refresh",
+    },
+    {
+      "<leader>ae",
+      function()
+        require("avante.api").edit()
+      end,
+      desc = "avante: edit",
+      mode = "v",
+    },
   },
   cmd = "AvanteAsk",
   build = "make",
   opts = {
-    provider = "copilot",
+    provider = "ollama",
+    vendors = {
+      ollama = {
+        ["local"] = true,
+        endpoint = "http://chat.groveer.com:11434/v1",
+        model = "llama3.1",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = H.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          H.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
   },
   dependencies = {
     "nvim-tree/nvim-web-devicons",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
     --- The below is optional, make sure to setup it properly if you have lazy=true
     {
       "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
     },
   },
 }
