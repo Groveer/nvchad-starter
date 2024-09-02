23eb8900354b844e44b02404411b797e1bc06662
groveer <groveer@outlook.com>
2024-09-03 01:57:15 +0800

    fix: error suda keymap in insert mode

--- lua/plugins/add/suda.lua
+++ lua/plugins/add/suda.lua
@@ -2,7 +2,7 @@ return {
   "lambdalisue/suda.vim",
   keys = {
     { "<A-s>", "<CMD>SudaWrite<CR>", "n", desc = "Suda Save file using sudo" },
-    { "<A-s>", "<Esc><CMD>SudaWrite<CR>", "n", desc = "Suda Save file using sudo" },
+    { "<A-s>", "<Esc><CMD>SudaWrite<CR>", "i", desc = "Suda Save file using sudo" },
   },
   cmd = { "SudaRead", "SudaWrite" },
   config = function()
